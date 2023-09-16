0d4c26e0ecbd57147d8e7738b62c0699f69de428
Vlad <vhv1000@gmail.com>
2023-09-17 00:58:59 +0200

    added basic definitions

---
+++ basic-definitions.scm
@@ -0,0 +1,16 @@
+(define-module (basic-definitions)
+  :export (atom?
+           member?))
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+
+(define member?
+  (lambda (a lat)
+    (cond
+     ((null? lat) #f)
+     ((eq? (car lat) a) #t)
+     (else (member? a (cdr lat))
+           ))))
