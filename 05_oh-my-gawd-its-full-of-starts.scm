a20a264380e7d92a32adf767c25e26f6175e8d9a
Vlad <vhv1000@gmail.com>
2023-10-01 09:24:59 +0200

    schemer: progress chapter 5

--- 05_oh-my-gawd-its-full-of-starts.scm
+++ 05_oh-my-gawd-its-full-of-starts.scm
@@ -8,11 +8,163 @@
             (cup)
             ))
 ;; what is
+;;
 (rember* a l)
+
 ;; => (
 ;;  (coffee)
 ;;  ((tea))
 ;;  (and (hick))
 ;; )
 ;; note: rember* is pronounced
-;; idiomatically rember-star
+;; idiomatically as rember-star
+
+(define a 'sauce)
+(define l '(((tomato sauce))
+            ((bean) sauce)
+            (and ((flying) sauce))
+            ))
+
+;; what is
+(rember* a l)
+
+;; =>
+;; (
+;;  ((tomato))
+;;  ((bean) sauce)
+;;  (and ((flying)))
+;; )
+
+;; now write rember*
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) '())
+     ((list? (car l))
+      (cons
+       (rember* a (car l))
+       (rember* a (cdr l))))
+     ((eq? a (car l))
+      (rember* a (cdr l)))
+     (else
+      (cons
+       (car l)
+       (rember* a (cdr l))))
+     )))
+
+;; what is
+(define new 'roast)
+(define old 'chuck)
+(define l '(
+           (how much (wood))
+           could
+           ((a (wood) chuck))
+           (((chuck)))
+           (if (a) ((wood chuck)))
+           could chuck wood
+            ))
+;; =>
+;; '(
+;;   (how much (wood))
+;;   could
+;;   ((a (wood) chuck roast))
+;;   (((chuck roast)))
+;;   (if (a) ((wood chuck roast)))
+;;   could chuck roast wood
+;;   )
+;;
+(insertR* new old l)
+
+;; write insertR*
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((list? (car l))
+      (cons
+       (insertR* new old (car l))
+       (insertR* new old (cdr l))))
+     ((eq? (car l) old)
+      (cons
+       (car l)
+       (cons new
+             (insertR* new old (cdr l)))))
+     (else (cons (car l)
+                 (insertR* new old (cdr l))))
+     )))
+
+;; alright, looks good
+;; test also works
+
+
+;; THE FIRST COMMANDMENT
+;; (final revision)
+;;
+;; When recurring on a list of atoms lat
+;; ask two questions about it
+;; (null? lat) and else
+;;
+;; When recurring on a number n, ask two questions
+;; about it: (zero? n) and else
+;;
+;; When recurring on a list of S-expressions "lists of lists" l
+;; ask three questions about it:
+;; (null? l) (atom? (car l)) and else
+;; in my case i ask 4, to make the cond more readable
+;;
+;; (null? l) (list? (car l)) (is-condition-met? (car l) condition) and else
+
+;; how are insertR* and rember* similar?
+;;
+;; each recurs on the (car l) if (car l) if (list? (car l)) => #t
+
+;; how are rember* and multirember different?
+;;
+;; multirember -> works only on flat lists
+;; rember* -> works on lists that are arbitrarily deep
+
+;; said in a more lispy way:
+;;
+;; rember* recurs on (car l) and (cdr l)
+;; multirember recurs only on (cdr l)
+
+;; how are all *-functions similar?
+;;
+;; they can be used on lists that are
+;; arbitrarily deep
+
+;; in the terms of the book and said in a more
+;; programmatic way
+;;
+;; they always ask 3 questions:
+;; -> (null? l)
+;; -> (list? (car l))
+;; -> else
+
+
+
+;; THE FOURTH COMMANDMENT
+;; (final revision)
+;;
+;; Always change at least one argument
+;; while recurring.
+;;
+;; When recurring on a list of atoms, use (cdr lat)
+;;
+;; When recurring on a number n, use (sub1 n)
+;;
+;; And when recurring on a list of S-expressios l, use (car l) and (cdr l)
+;; in case (list? (car l)) => #t
+;;
+;; the recursion must be changed with every iteration
+;; to be closer to termination. The chaning argument
+;; must be tested in the termination condition:
+;;
+;; when using cdr, test termination with null?
+;; when using sub1, test termination with zero?
+
+
+
+;; what is
